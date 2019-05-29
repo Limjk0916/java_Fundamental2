25a291a6d5017d467c89ede6afd66506112bc4fd
Limjk0916 <slslsle0916@gmail.com>
2019-05-29 18:38:25 +0900

    2019-05-29

---
+++ src/java_20190529/ClassDemo.java
@@ -0,0 +1,19 @@
+package java_20190529;
+
+public class ClassDemo {
+	class classDemo4{
+		
+	}
+}
+
+class ClassDemo2{
+	
+	
+}
+
+class ClassDemo3{
+	
+}
+
+//class 파일이 몇개 있어도 main은 하나
+//파일 하나당 class 하나 하는 것이 좋다.
---
+++ src/java_20190529/WildDemo.java
@@ -0,0 +1,26 @@
+package java_20190529;
+
+public class WildDemo {
+	public static void main(String[] args){
+		//Whie 1 ~100юг гу
+		int sum =0;
+		int i=0;
+		while(i<=100){
+			sum += i;
+			i++;
+		}
+		System.out.println("1 ~100 гу :"+sum);
+		
+		int j=2;
+		while(j<=9){
+			int k=1;
+			while(k<=9){
+				System.out.println(j+"*"+"="+(j*k));
+				k++;
+			}
+			j++;
+		}
+			
+	}
+
+}
